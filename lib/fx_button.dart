6988b5ed4759c7e2e282c14b6aa839e1dc39e522
gonzalopezzi <gonzalopezzi@gmail.com>
2015-04-28 12:10:19 +0200

    enabled property for buttons

--- lib/fx_button.dart
+++ lib/fx_button.dart
@@ -1,17 +1,24 @@
 import 'package:polymer/polymer.dart';
+import 'package:flex_components/fx_base.dart';
 import 'dart:html';
 
 @CustomTag('fx-button')
-class FxButton extends PolymerElement {
+class FxButton extends FxBase {
 
   @published String label;
   @observable bool pressed;
   @published String type;
+  @published bool enabled = true;
   
   /// Constructor used to create instance of FxButton.
   FxButton.created() : super.created() {
   }
   factory FxButton () => new Element.tag('fx-button');
+  
+  @override
+  void enabledChanged () {
+    invalidateProperties();
+  }
   
   void mouseDownHandler (Event e) {
     pressed = true;
@@ -19,6 +26,11 @@ class FxButton extends PolymerElement {
   
   void mouseUpHandler (Event e) {
     pressed = false;
+  }
+  
+  @override
+  void commitProperties () {
+    super.commitProperties();
   }
   
 }
